0b8670c4ab0c1cc78eca2327847c3e86cc1095ad
kisibokevin <kisibokevin@gmail.com>
2024-01-06 02:42:11 +0300

    added slide functionality to trending

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
 
-// const toggleIcons = document.querySelector('.toggle-icons');
-
 const openMenu = document.querySelector('.ri-menu-line');
 const closeMenu = document.querySelector('.ri-close-line');
 
 const menuIsOpen = document.querySelector('.menu');
 
-
-// menuIsOpen = false;
-/*
-toggleIcons.addEventListener('click', function() {
-
-    if(menuIsOpen) {
-        menuOpen.style.display="none"
-        menuClose.style.display="inline-block"
-        menuIsOpen.classList.toggle('active');
-    }
-    else
-    {
-        menuOpen.style.display="inline-block"
-        menuClose.style.display="none"
-        menuIsOpen.classList.remove('active');
-    }
-});
-*/
 
 openMenu.addEventListener('click', () => {
     openMenu.style.display="none"
@@ -36,3 +16,31 @@ closeMenu.addEventListener('click', () => {
     openMenu.style.display="inline-block"
     menuIsOpen.classList.remove('active');
 })
+
+const postsContainer = document.querySelector('.posts-container');
+
+function slidePosts() {
+
+    // clone the first post and append it to the end
+    const firstPost = postsContainer.firstElementChild.cloneNode(true);
+    postsContainer.appendChild(firstPost);
+
+    //animate the sliding effect
+    postsContainer.style.transition = "transform 0.5s ease-in-out";
+
+    //calculate the distance the posts should slide
+    const posts = postsContainer.querySelectorAll('.post');
+    const postsWidth = posts[0].offsetWidth;
+    postsContainer.style.transform = `translateX(-${postsWidth}px)`;
+
+    // remove first post after animation ends
+    setTimeout(() => {
+        postsContainer.style.transition = "none";
+        postsContainer.style.transform = `translateX(0)`;
+        postsContainer.removeChild(postsContainer.firstElementChild);
+    }, 1000);
+}
+    // call the slidePosts function every 3 seconds
+    setInterval(slidePosts, 3000);
+    
+    // you may adjust the time interval as needed
